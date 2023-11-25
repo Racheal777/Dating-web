f07f2b0f0781912d79137a8c66f3f6cdc026b4a0
Isaac <isaac.amankwaah@hubtel.com>
2023-11-25 23:10:36 +0000

    login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,24 +1,35 @@
-import AppFooter from "@/app/components/AppFooter";
-import AppButton from "../../components/AppButton";
-import bgImage from "../../assets/bg.png";
-import logo from '../../assets/logo.svg'
-import Image from "next/image";
+import Image from 'next/image'
+import AppInput from "@/app/components/AppInput";
+import AppButton from "@/app/components/AppButton";
 
 export default function Home() {
-  return (
-   
-            <form action="" className="  text-center" >
-        
+    return (
+        <div className="w-full px-20">
+            <form>
+                <div className={"form-control"}>
+                    <label className={"text-black"}>E-mail</label>
+                    <AppInput placeholder={"Enter e-mail"} name={"email"} />
+                </div>
 
-            <AppButton style={"white"} type={"submit"} text={"Login"} />
+                <div className={"form-control"}>
+                    <label className={"text-black"}>Password</label>
+                    <AppInput placeholder={"Enter password"} name={"password"} type={"password"}/>
+                </div>
 
-            <p className="text-black">Don't have an account? <a href="/auth/signup" className="text-red-600 px-2">Sign up</a></p>
+                <a className={"text-black float-right"} href={"#"}>Forgot Password</a>
 
-            <AppButton style={"white"} type={"submit"} text={"Continue with Google"}  />
+                <div className={"mt-20"}>
+                    <AppButton text={"Login"} />
+                </div>
+
             </form>
-          
-       
 
- 
-  );
+            <div className={"auth-footer"}>
+
+                <p className={"text-black"}>Don't have an account? <a href={"#"} className={"text-red-600"}>Sign up</a></p>
+
+                <AppButton backgroundColor={"black"} text={"Continue with Google"}/>
+            </div>
+        </div>
+    )
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,27 +4,29 @@ import AppButton from "@/app/components/AppButton";
 
 export default function Home() {
     return (
-        <div className="form_wrapper">
+        <div className="w-full px-20">
             <form>
                 <div className={"form-control"}>
-                    <label>E-mail</label>
+                    <label className={"text-black"}>E-mail</label>
                     <AppInput placeholder={"Enter e-mail"} name={"email"} />
                 </div>
 
                 <div className={"form-control"}>
-                    <label>Password</label>
+                    <label className={"text-black"}>Password</label>
                     <AppInput placeholder={"Enter password"} name={"password"} type={"password"}/>
                 </div>
 
-                <a className={"float-right"} href={"#"}>Forgot Password</a>
+                <a className={"text-black float-right"} href={"#"}>Forgot Password</a>
 
-                <AppButton className={"mt-20"} text={"Login"} />
+                <div className={"mt-20"}>
+                    <AppButton text={"Login"} />
+                </div>
 
             </form>
 
             <div className={"auth-footer"}>
 
-                <p>Don't have an account? <a href={"#"}>Sign up</a></p>
+                <p className={"text-black"}>Don't have an account? <a href={"#"} className={"text-red-600"}>Sign up</a></p>
 
                 <AppButton backgroundColor={"black"} text={"Continue with Google"}/>
             </div>
